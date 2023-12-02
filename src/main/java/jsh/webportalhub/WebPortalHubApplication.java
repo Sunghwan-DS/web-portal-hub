7e49d492dd75bd2064022d9047158cb5520952f0
Sunghwan-DS <jeonsung02@gmail.com>
2023-12-02 18:42:13 +0900

    Create Web Portal Hub project

---
+++ src/main/java/jsh/webportalhub/WebPortalHubApplication.java
@@ -0,0 +1,13 @@
+package jsh.webportalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPortalHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPortalHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jsh/webportalhub/WebPortalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.webportalhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortalHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
