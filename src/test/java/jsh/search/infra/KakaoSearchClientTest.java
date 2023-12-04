31e941fe23d92e7564a434987dbff2290aa8ffbb
Sunghwan-DS <jeonsung02@gmail.com>
2023-12-04 17:19:39 +0900

    Fill in the missing test DisplayName

--- src/test/java/jsh/search/infra/KakaoSearchClientTest.java
+++ src/test/java/jsh/search/infra/KakaoSearchClientTest.java
@@ -14,7 +14,7 @@ public class KakaoSearchClientTest {
     private KakaoSearchClient kakaoSearchClient;
 
     @Test
-    @DisplayName("")
+    @DisplayName("카카오 블로그 검색 API 테스트")
     void searchBlogTest() {
         var result = kakaoSearchClient.searchBlog("개발", "accuracy", 1, 10);
 
