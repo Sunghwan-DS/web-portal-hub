1e27bc829c36123783dec0fc42a315fa074d185e
Sunghwan-DS <jeonsung02@gmail.com>
2023-12-05 11:20:04 +0900

    Fix error that failed to receive datetime

--- src/main/java/jsh/search/domain/KakaoBlogDocument.java
+++ src/main/java/jsh/search/domain/KakaoBlogDocument.java
@@ -1,11 +1,14 @@
 package jsh.search.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public record KakaoBlogDocument(
     String blogname,
     String contents,
-    LocalDateTime dateTime,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    LocalDateTime datetime,
     String thumbnail,
     String title,
     String url
